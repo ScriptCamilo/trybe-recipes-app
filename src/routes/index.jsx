0984402d4a56af66b5e47be4e9e9056467c74f33
ScriptCamilo <script.camilo@gmail.com>
2021-07-20 08:53:13 -0300

    style: removed unused styles in router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,17 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import routes from './routesData';
 
-import styles from './routes.module.scss';
-
 function Routes() {
   return (
-    <div className={ styles.container }>
-      <Switch>
-        {routes.map((route) => (
-          <Route key={ route.path } { ...route } />
-        ))}
-      </Switch>
-    </div>
+    <Switch>
+      {routes.map((route) => (
+        <Route key={ route.path } { ...route } />
+      ))}
+    </Switch>
   );
 }
 
