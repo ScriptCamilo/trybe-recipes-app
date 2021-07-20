4f012cb325da023f9ccb8ccce6b01a75a02aa97f
ScriptCamilo <script.camilo@gmail.com>
2021-07-20 19:31:23 -0300

    style: header styles and new icons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Proptypes from 'prop-types';
 import { Link } from 'react-router-dom';
+// import { AiOutlineFileSearch } from 'react-icons/ai';
+import { GiArchiveResearch, GiCook } from 'react-icons/gi';
+// import { SiCodechef } from 'react-icons/si';
 
-import profileIcon from '../../images/profileIcon.svg';
-import searchIcon from '../../images/searchIcon.svg';
+// import profileIcon from '../../images/profileIcon.svg';
+// import searchIcon from '../../images/searchIcon.svg';
 import Search from '../Search';
 
-import '../../styles/global.scss';
+import styles from './header.module.scss';
 
 function Header({ title, icon, currentPage }) {
   const [isSearch, setIsSearch] = useState(false);
@@ -16,15 +19,21 @@ function Header({ title, icon, currentPage }) {
 
   return (
     <>
-      <header className="#header-container">
+      <header className={ styles.header }>
         <Link to="/perfil">
-          <img src={ profileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
+          { /*
+            <img src={ profileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
+          */}
+          <GiCook size="3rem" className={ styles.headerIcon } />
         </Link>
         <h1 data-testid="page-title">{ title }</h1>
         {
-          (icon === 'false') ? <div /> : (
+          (icon === 'false') ? <div className={ styles.adjust } /> : (
             <button type="button" onClick={ handleSearch }>
-              <img src={ searchIcon } alt="Search Icon" data-testid="search-top-btn" />
+              {/*
+                <img src={ searchIcon } alt="Search Icon" data-testid="search-top-btn" />
+              */}
+              <GiArchiveResearch size="3rem" className={ styles.headerIcon } />
             </button>
           )
         }
