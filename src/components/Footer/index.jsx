7a757b4eb8df81d70a04f4ddd3e13f5d771d80c2
ScriptCamilo <script.camilo@gmail.com>
2021-07-20 19:30:35 -0300

    style: footer styles and new icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import drinkIcon from '../../images/drinkIcon.svg';
-import exploreIcon from '../../images/exploreIcon.svg';
-import mealIcon from '../../images/mealIcon.svg';
+import { NavLink } from 'react-router-dom';
+import {
+  GiHotMeal,
+  GiMartini,
+  GiStarSattelites,
+  // GiBooze,
+  // GiGlassCelebration,
+  // GiWineBottle,
+  // GiWineGlass,
+  // GiCompass,
+  // GiOrbital,
+  // GiRadarSweep,
+} from 'react-icons/gi';
+
+// martini, save, radar
+
+// import drinkIcon from '../../images/drinkIcon.svg';
+// import exploreIcon from '../../images/exploreIcon.svg';
+// import mealIcon from '../../images/mealIcon.svg';
+
 import styles from './footer.module.scss';
 
 function Footer() {
   return (
     <footer className={ styles.footer } data-testid="footer">
-      <Link to="/bebidas">
-        <img
+      <NavLink strict to="/bebidas" activeClassName={ styles.navBarLink }>
+        {/* <img
           alt="Drinks"
           src={ drinkIcon }
           data-testid="drinks-bottom-btn"
-        />
-      </Link>
-      <Link to="/explorar">
-        <img
+        /> */}
+        <GiMartini size="3rem" />
+      </NavLink>
+      <NavLink strict to="/explorar" activeClassName={ styles.navBarLink }>
+        {/* <img
           alt="Explorar"
           src={ exploreIcon }
           data-testid="explore-bottom-btn"
-        />
-      </Link>
-      <Link to="/comidas">
-        <img
+        /> */}
+        <GiStarSattelites size="3rem" />
+      </NavLink>
+      <NavLink strict to="/comidas" activeClassName={ styles.navBarLink }>
+        {/* <img
           alt="Comidas"
           src={ mealIcon }
           data-testid="food-bottom-btn"
-        />
-      </Link>
+        /> */}
+        <GiHotMeal size="3rem" />
+      </NavLink>
     </footer>
   );
 }
